3e63ae0ddaad40a70be11c464ab03c9d10fd52d7
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-08-12 15:27:09 -0300

    Criação da tela de Pesquisa

--- cobranca/src/main/java/com/gora/cobranca/controller/CadastroTituloController.java
+++ cobranca/src/main/java/com/gora/cobranca/controller/CadastroTituloController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,6 +37,11 @@ public class CadastroTituloController {
 		return modelView;
 	}
 
+	@GetMapping
+	public String pesquisar() {
+		return "PesquisaTitulo";
+	}
+	
 	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> retornaStatusTitulo(){
