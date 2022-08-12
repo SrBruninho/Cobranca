822280d12eed4aa2a0de3a9cf3dd0adb7d2195ad
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-08-12 14:33:51 -0300

    Combo Dinâmico e inserção do mySQL com Docker

--- cobranca/src/main/java/com/gora/cobranca/controller/CadastroTituloController.java
+++ cobranca/src/main/java/com/gora/cobranca/controller/CadastroTituloController.java
@@ -1,11 +1,16 @@
 package com.gora.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gora.cobranca.model.StatusTitulo;
 import com.gora.cobranca.model.Titulo;
 import com.gora.cobranca.repository.TituloRepository;
 
@@ -17,8 +22,10 @@ public class CadastroTituloController {
 	private TituloRepository tituloRepository;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView modelView = new ModelAndView( "CadastroTitulo" );
+		modelView.addObject( "todosStatusTitulo", StatusTitulo.values() );
+		return modelView;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST )
@@ -29,4 +36,9 @@ public class CadastroTituloController {
 		return modelView;
 	}
 
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> retornaStatusTitulo(){
+		return Arrays.asList( StatusTitulo.values() );
+	}
 }
