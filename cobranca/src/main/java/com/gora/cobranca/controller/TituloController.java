2396e66a8dc50addb8676533daf6b5678125551b
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-08-18 12:50:28 -0300

    Implementação da exclusão

--- cobranca/src/main/java/com/gora/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/gora/cobranca/controller/TituloController.java
@@ -2,13 +2,16 @@ package com.gora.cobranca.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,12 +26,18 @@ import com.gora.cobranca.repository.TituloRepository;
 @RequestMapping("/titulos")
 public class TituloController {
 	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	private static final String PESQUISA_VIEW = "PesquisaTitulo";
+	private static final String REDIRECT_CADASTRO_VIEW = "redirect:/titulos/novo";
+	private static final String REDIRECT_PESQUISA_VIEW = "redirect:/titulos";
+	
+	
 	@Autowired
 	private TituloRepository tituloRepository;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView modelView = new ModelAndView( "CadastroTitulo" );
+		ModelAndView modelView = new ModelAndView( CADASTRO_VIEW );
 		modelView.addObject( "todosStatusTitulo", StatusTitulo.values() );
 		modelView.addObject( new Titulo() );
 		return modelView;
@@ -37,21 +46,34 @@ public class TituloController {
 	@PostMapping
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes redAttributes ) {
 		if( errors.hasErrors() )
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		
 		tituloRepository.save( titulo );
 		redAttributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		return REDIRECT_CADASTRO_VIEW;
 	}
 
-	@GetMapping
-	public ModelAndView pesquisar() {
-		List<Titulo> listTitulos = tituloRepository.findAll();
-		ModelAndView modelView = new ModelAndView( "PesquisaTitulo" );
-		modelView.addObject( "titulos", listTitulos );
+	@RequestMapping("{codigo}")
+	public ModelAndView editar( @PathVariable("codigo") Titulo titulo ) {
+		ModelAndView modelView = new ModelAndView( CADASTRO_VIEW );
+		modelView.addObject( titulo );
 		return modelView;
 	}
 	
+	@DeleteMapping("{codigo}")
+	public String deletar(@PathVariable Long codigo, RedirectAttributes redAttributes) {
+		tituloRepository.deleteById( codigo );
+		redAttributes.addFlashAttribute("mensagem","Título excluído com sucesso!");
+		return REDIRECT_PESQUISA_VIEW;
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		List<Titulo> listTitulos = tituloRepository.findAll();
+		ModelAndView modelView = new ModelAndView( PESQUISA_VIEW );
+		modelView.addObject( "titulos", listTitulos );
+		return modelView;
+	}	
 	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> retornaStatusTitulo(){
