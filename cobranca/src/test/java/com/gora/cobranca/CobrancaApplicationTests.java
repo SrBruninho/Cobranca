c0cd1c93fbcd9a3ab1ae28c754d5d1370e853b57
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-08-20 01:58:19 -0300

    Ajuste MainTest

--- cobranca/src/test/java/com/gora/cobranca/CobrancaApplicationTests.java
+++ cobranca/src/test/java/com/gora/cobranca/CobrancaApplicationTests.java
@@ -1,9 +1,8 @@
 package com.gora.cobranca;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CobrancaApplicationTests {
 
 	@Test
